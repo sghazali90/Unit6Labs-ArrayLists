b59eb0fd6f6c230d97df2d3f47087ceb72b5a3f1
sghazali90 <59652139+sghazali90@users.noreply.github.com>
2020-03-16 13:17:33 -0400

    Create CardGame.java

---
+++ CardGame.java
@@ -0,0 +1,14 @@
+/**
+ * Class CardGame
+ */
+
+import java.util.*;
+
+public class CardGame {
+	public static void main(String[] args) {
+    	
+    	Card card1= new Card("Diamonds",14);
+    	System.out.println(card1);
+  
+	}
+}
